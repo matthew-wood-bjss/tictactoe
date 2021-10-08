4157c95534ec2380a3c4466e05048a1d051101e0
matt wood <matthew.wood@bjss.com>
2021-10-08 14:13:17 +0100

    Added docstrings to functions

--- tictactoetest/tictactoetest/tictactoe.py
+++ tictactoetest/tictactoetest/tictactoe.py
@@ -1,5 +1,9 @@
-
 def user_setup():
+    '''Function that sets up the game by giving Player 1 the choice of being Token X or O. If an invalid choice is given, try again.
+    Args:       None
+
+    Returns:    setup (string): Player 1's token
+                setup2 (string): Player 2's token'''
     setup = str(input("Player 1, please choose X or O\n")).upper()
     while (setup != 'X' and setup != 'O'):
         setup = str(input("Invalid choice, Player 1, please choose X or O\n")).upper()
@@ -12,6 +16,15 @@ def user_setup():
     return setup,setup2
 
 def user_choice(isplayer1,board,p1,p2):
+    '''Function that gives the respective player's turn the choice of where to go on the board, updates said board and changes whose turn it is.
+    If an invalid choice is given, try again.
+    Args:       isplayer1 (bool): is it Player 1's turn?
+                board (list): current board state containing either "-"s, "X"s or "O"s
+                p1 (string): Player 1's token
+                p2 (string): Player 2's token
+
+    Returns:    isplayer1 (bool): is it Player 1's turn? (should be opposite to the argument inputted to this function)
+                board (list): new board state containing either "-"s, "X"s or "O"s'''
     grid = list(range(10))
     if isplayer1:
         player = 1
@@ -30,7 +43,14 @@ def user_choice(isplayer1,board,p1,p2):
     isplayer1 = not isplayer1
     return isplayer1,board
 
+
 def is_winner(board,isplayer1):
+    '''Function that checks if there is any winner with the current board state. If there is, announce the player who just made their turn as
+    the winner, if there is a draw also announce there is a draw.
+    Args:       isplayer1 (bool): is it Player 1's turn?
+                board (list): current board state containing either "-"s, "X"s or "O"s
+
+    Returns:    (bool): Should the game be over?'''
     wins = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
     for i in wins:
         if all(['X' == board[j] for j in i]) or all(['O' == board[j] for j in i]):
@@ -47,6 +67,10 @@ def is_winner(board,isplayer1):
             
             
 def main():
+    '''Function to start the game. continues to play and only ends if someone has won the game or there is a draw.
+    Args:       None
+
+    Returns:    None'''
     isplayer1 = True
     board = ['-' for i in range(9)]
     print("Current board state:\n{0}\n{1}\n{2}\n".format(board[0:3],board[3:6],board[6:9]))
