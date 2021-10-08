9c6205004ed00b2e4795dc846e0a8a393b0b58ef
matt wood <matthew.wood@bjss.com>
2021-10-08 14:59:04 +0100

    Added first initial test to test setup

--- tictactoetest/tests/test_usersetup.py
+++ tictactoetest/tests/test_usersetup.py
@@ -1,5 +1,13 @@
+from unittest.mock import patch
 from tictactoetest import __version__
 from tictactoetest.tictactoe import user_setup
 
+
 def test_version():
     assert __version__ == '0.1.0'
+
+@patch('builtins.input', return_value='X')
+def test_setup_X(mock_input):
+    '''Should return player 1 as X if X is given'''
+    player1choice = user_setup()
+    assert player1choice == ('X','O')
