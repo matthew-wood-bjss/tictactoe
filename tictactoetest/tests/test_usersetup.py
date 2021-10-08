e8d03a57a5c6d2585df3dd45e3c29e02e5f339b6
matt wood <matthew.wood@bjss.com>
2021-10-08 15:20:08 +0100

    Tests for both O and X inputs; can't test for other inputs otherwise gets stuck in while loop

--- tictactoetest/tests/test_usersetup.py
+++ tictactoetest/tests/test_usersetup.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch
+from unittest.mock import patch, call
 from tictactoetest import __version__
 from tictactoetest.tictactoe import user_setup
 
@@ -8,6 +8,12 @@ def test_version():
 
 @patch('builtins.input', return_value='X')
 def test_setup_X(mock_input):
-    '''Should return player 1 as X if X is given'''
+    '''Should return player 1 as X and player 2 as O if X is given'''
     player1choice = user_setup()
     assert player1choice == ('X','O')
+
+@patch('builtins.input', return_value='O')
+def test_setup_O(mock_input):
+    '''Should return player 1 as O and player 2 as X if O is given'''
+    player1choice = user_setup()
+    assert player1choice == ('O','X')
